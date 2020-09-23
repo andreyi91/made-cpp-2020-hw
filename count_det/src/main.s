536dee127461c527d478024fecf56236db5c4445
andreyi91 <AndreyI-91@yandex.ru>
2020-09-23 19:41:54 +0300

    added solution for hw_2

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,36 @@
-/// Your code goes here...
+.text
 
+.global count_det
+
+count_det:
+	ldrsb r1, [r0], #4
+	mov r6, r1
+	ldrsb r3, [r0], #4
+	mov r5, r3
+	ldrsb r2, [r0], #4
+	mov r4, r2
+	ldrsb r7, [r0], #4
+	mul r2, r2, r7
+	mul r5, r5, r7
+	ldrsb r7, [r0], #4
+	mul r1, r1, r7
+	mul r4, r4, r7
+	ldrsb r7, [r0], #4
+	mul r3, r3, r7
+	mul r6, r6, r7
+	ldrsb r7, [r0], #4
+        mul r3, r3, r7
+        mul r4, r4, r7
+        ldrsb r7, [r0], #4
+        mul r2, r2, r7
+        mul r6, r6, r7
+        ldrsb r7, [r0], #4
+        mul r1, r1, r7
+        mul r5, r5, r7
+
+	add r0, r1, r2
+	add r0, r3
+	sub r0, r4
+	sub r0, r5
+	sub r0, r6
+	bx lr	
