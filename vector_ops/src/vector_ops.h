8c4846f0096c5dc33f06d7571017818ab761123a
andreyi91 <AndreyI-91@yandex.ru>
2020-09-23 21:00:26 +0300

    vector_ops updates 1

--- vector_ops/src/vector_ops.h
+++ vector_ops/src/vector_ops.h
@@ -1,11 +1,51 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <cstdint>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
 
 
 namespace task {
 
 // Your code here...
+
+vector<int> scan_vec () {
+	size_t N;
+	cin >> N;
+
+	vector<int> arr;
+
+	for (size_t i = 0; i < N; ++ i) {
+		int x;
+		cin >> x;
+		arr.push_back(x);
+	}
+	return arr;
+
+}
+
+void print_vec (vector<int> x) {
+	for (size_t i = 0; i < x.size(); ++ i) {
+		cout << x[i] << " ";
+	}
+
+	cout << endl;
+}
+
+int main() {
+	vector<int> arr = scan_vec();
+	print_vec(arr);
+	//arr.push_back(123);
+	//arr.push_back(456);
+	//arr.pop_back();
+
+
+	return 0;
+}
 
 
 }  // namespace task
